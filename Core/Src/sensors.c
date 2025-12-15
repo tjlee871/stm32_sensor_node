b93f6c1b77fc798ad1b05fb067e5a0b624833218
tjlee871 <tjlee871@gmail.com>
2025-12-15 15:04:20 -0500

    sensor function return updated

--- Core/Src/sensors.c
+++ Core/Src/sensors.c
@@ -9,33 +9,38 @@
 
 HAL_StatusTypeDef hts221_init(I2C_HandleTypeDef *handler)
 {
-  uint8_t buffer[16] = {0};
+  uint8_t buffer[3] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
 
-  if (HAL_I2C_Mem_Read(handler, HTS221_READ, HTS221_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, HTS221_READ, HTS221_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   if (buffer[0] == 0xBC)
   {
     buffer[0] = 0x1B;	//AVGT = 0b011, AVGH = 0b011
-    if (HAL_I2C_Mem_Write(handler, HTS221_WRITE, HTS221_AV_CONF, 1, buffer, 1, SENSOR_COM_TIMEOUT))
+    status = HAL_I2C_Mem_Write(handler, HTS221_WRITE, HTS221_AV_CONF, 1, buffer, 1, SENSOR_COM_TIMEOUT);
+    if (status != HAL_OK)
     {
-      return HAL_ERROR;
+      return status;
     }
 
     buffer[0] = 0x83;	//PD = active mode, ODR = 12.5Hz
-    if (HAL_I2C_Mem_Write(handler, HTS221_WRITE, HTS221_CTRL_REG, 1, buffer, 3, SENSOR_COM_TIMEOUT))
+    status = HAL_I2C_Mem_Write(handler, HTS221_WRITE, HTS221_CTRL_REG, 1, buffer, 3, SENSOR_COM_TIMEOUT);
+    if (status != HAL_OK)
     {
-      return HAL_ERROR;
+      return status;
     }
   }
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef hts221_read(I2C_HandleTypeDef *handler, hts221_typedef *data)
 {
   uint8_t buffer[16] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
 
   uint8_t H0_RH_cal = 0;
   uint8_t H1_RH_cal = 0;
@@ -49,17 +54,19 @@ HAL_StatusTypeDef hts221_read(I2C_HandleTypeDef *handler, hts221_typedef *data)
   int16_t T1_OUT_cal = 0;
   int16_t T_OUT_meas = 0;
 
-  if (HAL_I2C_Mem_Read(handler, HTS221_READ, HTS221_DATA_OUT, 1, buffer, 5, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, HTS221_READ, HTS221_DATA_OUT, 1, buffer, 5, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   H_OUT_meas = (buffer[2] << 8) | buffer[1];
   T_OUT_meas = (buffer[4] << 8) | buffer[3];
 
-  if (HAL_I2C_Mem_Read(handler, HTS221_READ, HTS221_CALIBRATION_REG, 1, buffer, 16, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, HTS221_READ, HTS221_CALIBRATION_REG, 1, buffer, 16, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   H0_RH_cal = buffer[0] >> 1;
@@ -77,39 +84,45 @@ HAL_StatusTypeDef hts221_read(I2C_HandleTypeDef *handler, hts221_typedef *data)
   data->temperatureC = (float)(T_OUT_meas - T0_OUT_cal) * (float)(T1_DegC_cal - T0_DegC_cal) / (float)(T1_OUT_cal - T0_OUT_cal) + (float)T0_DegC_cal;
   data->temperatureF = (data->temperatureC * 1.8) + 32;
 
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef lis3mdl_init(I2C_HandleTypeDef *handler)
 {
-  uint8_t buffer[16] = {0};
+  uint8_t buffer[5] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
 
-  if (HAL_I2C_Mem_Read(handler, LIS3MDL_READ, LIS3MDL_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, LIS3MDL_READ, LIS3MDL_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   if (buffer[0] == 0x3D)
   {
     buffer[0] = 0x10;	//DO = 0b100
-    if (HAL_I2C_Mem_Write(handler, LIS3MDL_WRITE, LIS3MDL_CTRL_REG, 1, buffer, 5, SENSOR_COM_TIMEOUT))
+    status = HAL_I2C_Mem_Write(handler, LIS3MDL_WRITE, LIS3MDL_CTRL_REG, 1, buffer, 5, SENSOR_COM_TIMEOUT);
+    if (status != HAL_OK)
     {
-      return HAL_ERROR;
+      return status;
     }
   }
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef lis3mdl_read(I2C_HandleTypeDef *handler, lis3mdl_typedef *data)
 {
-  uint8_t buffer[16] = {0};
+  uint8_t buffer[9] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
+
   int16_t raw_x = 0;
   int16_t raw_y = 0;
   int16_t raw_z = 0;
 
-  if (HAL_I2C_Mem_Read(handler, LIS3MDL_READ, LIS3MDL_DATA_OUT, 1, buffer, 9, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, LIS3MDL_READ, LIS3MDL_DATA_OUT, 1, buffer, 9, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   raw_x = (buffer[2] << 8) | buffer[1];
@@ -120,16 +133,18 @@ HAL_StatusTypeDef lis3mdl_read(I2C_HandleTypeDef *handler, lis3mdl_typedef *data
   data->y = (float)raw_y * 0.146;
   data->z = (float)raw_z * 0.146;
 
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef lps22hb_init(I2C_HandleTypeDef *handler)
 {
-  uint8_t buffer[16] = {0};
+  uint8_t buffer[3] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
 
-  if (HAL_I2C_Mem_Read(handler, LPS22HB_READ, LPS22HB_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, LPS22HB_READ, LPS22HB_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   if (buffer[0] == 0xB1)
@@ -137,38 +152,44 @@ HAL_StatusTypeDef lps22hb_init(I2C_HandleTypeDef *handler)
     buffer[0] = 0x30;	//ODR = 25Hz
     buffer[1] = 0x10;	//IF_ADD_INC = enable
     buffer[2] = 0x00;
-    if (HAL_I2C_Mem_Write(handler, LPS22HB_WRITE, LPS22HB_CTRL_REG, 1, buffer, 3, SENSOR_COM_TIMEOUT))
+    status = HAL_I2C_Mem_Write(handler, LPS22HB_WRITE, LPS22HB_CTRL_REG, 1, buffer, 3, SENSOR_COM_TIMEOUT);
+    if (status != HAL_OK)
     {
-      return HAL_ERROR;
+      return status;
     }
   }
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef lps22hb_read(I2C_HandleTypeDef *handler, lps22hb_typedef *data)
 {
-  uint8_t buffer[16] = {0};
+  uint8_t buffer[4] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
+
   int32_t raw_pressure = 0;
 
-  if (HAL_I2C_Mem_Read(handler, LPS22HB_READ, LPS22HB_DATA_OUT, 1, buffer, 4, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, LPS22HB_READ, LPS22HB_DATA_OUT, 1, buffer, 4, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   raw_pressure = (buffer[3] << 16) | (buffer[2] << 8) | buffer[1];
 
   data->pressure = (float)raw_pressure / 4096;
 
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef lsm6dsl_init(I2C_HandleTypeDef *handler)
 {
-  uint8_t buffer[16] = {0};
+  uint8_t buffer[3] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
 
-  if (HAL_I2C_Mem_Read(handler, LSM6DSL_READ, LSM6DSL_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, LSM6DSL_READ, LSM6DSL_WHO_AM_I, 1, buffer, 1, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   if (buffer[0] == 0x6A)
@@ -176,17 +197,20 @@ HAL_StatusTypeDef lsm6dsl_init(I2C_HandleTypeDef *handler)
     buffer[0] = 0x10;	//ODR_XL = 12.5Hz
     buffer[1] = 0x10;	//ODR_G = 12.5Hz
     buffer[2] = 0x44;	//BDU = continuous update, IF_INC = enable
-    if (HAL_I2C_Mem_Write(handler, LSM6DSL_WRITE, LSM6DSL_CTRL_REG, 1, buffer, 3, SENSOR_COM_TIMEOUT))
+    status = HAL_I2C_Mem_Write(handler, LSM6DSL_WRITE, LSM6DSL_CTRL_REG, 1, buffer, 3, SENSOR_COM_TIMEOUT);
+    if (status != HAL_OK)
     {
-      return HAL_ERROR;
+      return status;
     }
   }
-  return HAL_OK;
+  return status;
 }
 
 HAL_StatusTypeDef lsm6dsl_read(I2C_HandleTypeDef *handler, lsm6dsl_typedef *data)
 {
   uint8_t buffer[16] = {0};
+  HAL_StatusTypeDef status = HAL_OK;
+
   int16_t raw_gyro_x = 0;
   int16_t raw_gyro_y = 0;
   int16_t raw_gyro_z = 0;
@@ -194,9 +218,10 @@ HAL_StatusTypeDef lsm6dsl_read(I2C_HandleTypeDef *handler, lsm6dsl_typedef *data
   int16_t raw_acc_y = 0;
   int16_t raw_acc_z = 0;
 
-  if (HAL_I2C_Mem_Read(handler, LSM6DSL_READ, LSM6DSL_DATA_OUT, 1, buffer, 16, SENSOR_COM_TIMEOUT))
+  status = HAL_I2C_Mem_Read(handler, LSM6DSL_READ, LSM6DSL_DATA_OUT, 1, buffer, 16, SENSOR_COM_TIMEOUT);
+  if (status != HAL_OK)
   {
-    return HAL_ERROR;
+    return status;
   }
 
   raw_gyro_x = (buffer[5] << 8) | buffer[4];
@@ -213,5 +238,5 @@ HAL_StatusTypeDef lsm6dsl_read(I2C_HandleTypeDef *handler, lsm6dsl_typedef *data
   data->acc_y = (float)raw_acc_y * 0.061;
   data->acc_z = (float)raw_acc_z * 0.061;
 
-  return HAL_OK;
+  return status;
 }
